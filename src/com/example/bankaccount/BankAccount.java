b14d958d34bc2cc6bc0b3c65b6b928fac17d5622
roesnera <adam.roesner@generaiton.org>
2022-07-07 12:42:44 -0400

    Creates file structure, BankAccount and Main classes, adds instructions in comments

---
+++ src/com/example/bankaccount/BankAccount.java
@@ -0,0 +1,75 @@
+package com.example.bankaccount;
+
+public class BankAccount {
+    /*
+        RULES:
+        1. No direct souts in our methods, only returns
+        2. Do not allow for negative balances!
+
+        Requirements:
+            Instance fields(data type):
+                balance(double)
+                nameOnAccount(String)
+                accountNumber(int)
+                accountType(String)
+
+            Class methods(return types):
+                withdraw(boolean)
+                deposit(boolean)
+                constructor
+                getters(field type)
+                toString(String)
+     */
+
+    /*
+        Instance field declarations should go here
+        balance(double), nameOnAccount(String), accountNumber(int), accountType(String)
+        These fields should not be able to be manipulated or access outside this class!
+     */
+
+
+    /*
+        Constructor method should go here
+        remember to take proper input for each instance field
+        then, assign that input to the appropriate field
+     */
+
+
+    /*
+        All your getter methods can go below
+        The return type for your getter methods depends on which instance field you are getting!
+     */
+
+
+    /*
+        Here you should write your withdraw method
+        requirements:
+            1. no negatives as input
+            2. depending on accountType:
+                calculate transaction fee of $1.50 (checking only)
+                calculate interest of 1% on withdrawn amount (savings only)
+            3. check balance
+            4. return true if successful
+                otherwise, return false
+     */
+
+
+    /*
+        Here you should write your deposit method
+        requirements:
+            1. no negatives as input
+            2. do not allow for overdrafting
+            3. return true if successful
+                otherwise, return false
+     */
+
+
+    /*
+        Here you should write you toString method
+        requirements:
+            1. returns a String that represents all your instance field values
+     */
+
+
+
+}
---
+++ src/com/example/main/Main.java
@@ -0,0 +1,22 @@
+package com.example.main;
+
+// import statement for our BankAccount class
+import com.example.bankaccount.BankAccount;
+
+public class Main {
+    /*
+        Your main method here should create two instances of BankAccount
+            one as a savings type
+            one as a checkings type
+
+        Call each method of each object instance to verify that they work as intended
+
+        If you have extra time, check for invalid input as well (negative numbers, overdrafts, etc.)
+            we do this to ensure that our class handles input as expected
+            this means that it also has to handle invalid input appropriately
+     */
+    public static void main(String[] args) {
+
+
+    }
+}
